4590fd7169958575bf29de5f0a2f24d5fa3ce36e
Andrzej Szelemetko <a.szelemetko@gmail.com>
2018-12-05 17:02:55 +0100

    added route guard for editing and adding new recipe

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import * as firebase from 'firebase';
+import {Router} from '@angular/router';
+import {Injectable} from '@angular/core';
 
+@Injectable()
 export class AuthService {
   token: string;
+
+  constructor(private router: Router) {}
+
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -17,10 +23,11 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
-        firebase.auth().currentUser.getIdToken()
-          .then(
-            (token: string) => this.token = token
-          );
+          this.router.navigate(['/']);
+          firebase.auth().currentUser.getIdToken()
+            .then(
+              (token: string) => this.token = token
+            );
         })
       .catch(
         error => console.log(error)
