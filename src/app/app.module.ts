a8ed92f5c662921fc571c907c8795d1eb954824d
Andrzej Szelemetko <a.szelemetko@gmail.com>
2018-12-08 14:07:40 +0100

    added first acions, reducers, moved shopping list to state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
 import {CoreModule} from './core/core.module';
 import {HttpClientModule} from '@angular/common/http';
+import {StoreModule} from '@ngrx/store';
+import {shoppingListReducer} from './shopping-list/store/shopping-list.reducers';
 
 
 @NgModule({
@@ -22,6 +24,9 @@ import {HttpClientModule} from '@angular/common/http';
     AppRoutingModule,
     SharedModule,
     CoreModule,
+    StoreModule.forRoot({
+      shoppingList: shoppingListReducer
+    })
   ],
 
   bootstrap: [AppComponent]
